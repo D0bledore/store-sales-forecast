f56be75bd4da94494a3c1ca50e26ce0165bab08a
d0bledore <verime@duck.com>
2025-05-21 10:47:31 +0200

    Fix Render deployment error: corrected stores file URL

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.title("Sales Forecast Dashboard - Corporación Favorita")
 # Load data
 forecast_df = pd.read_csv("https://raw.githubusercontent.com/d0bledore/store-sales-forecast/main/forecasts/forecast_results_sample.csv")
 actual_df = pd.read_csv("https://raw.githubusercontent.com/d0bledore/store-sales-forecast/main/data/train_sample.csv")
-store_meta = pd.read_csv("https://raw.githubusercontent.com/d0bledore/store-sales-forecast/main/data/stores_sample.csv")
+store_meta = pd.read_csv("https://raw.githubusercontent.com/d0bledore/store-sales-forecast/main/data/stores.csv")
 full_train = pd.read_csv("https://raw.githubusercontent.com/d0bledore/store-sales-forecast/main/data/processed/inventory_prepared_sample.csv")
 
 
